753a8047bce09b93370de149050d38a08350d20e
Phentecost <93055187+Phentecost@users.noreply.github.com>
2021-11-04 07:23:51 -0500

    Add files via upload

---
+++ Llave.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Llave : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerController Player = collision.gameObject.GetComponent<PlayerController>();
+
+        if (Player != null)
+        {
+            Player.LlavePuerta = true;
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Puerta.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puerta : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        PlayerController Player = collision.gameObject.GetComponent<PlayerController>();
+
+        if (Player != null)
+        {
+            if (Player.LlavePuerta)
+            {
+                CanvasManager Canva = GameObject.Find("Canvas").GetComponent<CanvasManager>();
+                Canva.Ganaste = true;
+            }
+        }
+    }
+
+}
